f2e8144feed7922d21eeceda11f383acdb342b61
Bozio96 <rodrigobozio96@gmail.com>
2022-11-07 15:19:14 -0300

    Segunda entrega de js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,67 @@
 //Declaracion de variables
 let edad;
+let totalCarrito = 0;
+
+//Arrays y objetos
+const productos = [
+    {
+        id: 1,
+        nombre: "ipa",
+        categoria: "cerveza",
+        precio: 450
+    },
+    {
+        id: 2,
+        nombre: "golden",
+        categoria: "cerveza",
+        precio: 300
+    },
+    {
+        id: 3,
+        nombre: "honey",
+        categoria: "cerveza",
+        precio: 350
+    }
+]
+const carrito = [];
 
 //Declaracion de funciones
-
 function ingreso(b){
     for(let i=2; i>=0; i--){
         b = Number(prompt("Ingrese su edad")); 
         while(isNaN(b)){
             b = Number(prompt("Por favor ingrese una edad válida"));
         }
-        if(b >= 18){
+        if(b >= 18 && b <= 122){ //La persona más longeva de la historia ‒una mujer francesa llamada Jeanne Calment ‒ vivió hasta los 122. Cuando nació en 1875, la esperanza media de vida era de unos 43
             alert("Cumplis los requisitos!");
             let res = prompt("Deseas comprar algo? S/N");
             while(res !== "s" && res !== "S" && res !== "n" && res !== "N"){
                 res = prompt("Por favor ingrese una opción válida");
             }
-            let compra = 0;
             if(res === "s" || res === "S"){
-                let opcion = prompt("Elegí una de nuestras opciones de cervezas: \n1- IPA: $400 \n2 - GOLDEN: $300 \n3 - HONEY: $350 \nPresioná X para finalizar.");
+                let existe;
+                let opcion = prompt("Elegí una de nuestras opciones de cervezas: \n" + productos[0].nombre.toUpperCase() + ": $" + productos[0].precio + " \n" + productos[1].nombre.toUpperCase() + ": $" + productos[1].precio + " \n" + productos[2].nombre.toUpperCase() + ": $" + productos[2].precio + " \n Presioná X para finalizar.");
                 while (opcion != "X" && opcion != "x") {
-                         switch (opcion) {
-                           case "1":
-                             alert("Elegiste IPA");
-                             compra += 400;
-                            break;
-                           case "2":
-                            alert("Elegiste GOLDEN");
-                            compra += 300; 
-                            break;
-                           case "3":
-                             alert("Elegiste HONEY");
-                             compra += 350;
-                             break;
-                    
-                           default:
-                             alert("Opción no válida");
-                             break;
-                         }
-                         opcion = prompt("Desea agregar algo más? \n1- IPA: $400 \n2 - GOLDEN: $300 \n3 - HONEY: $350 \nPresioná X para finalizar.");
-                       }
-                alert("Tu compra final es de $ " + compra);
+                    existe = productos.some((producto)=>{
+                        return producto.nombre === opcion.toLowerCase();
+                      })
+                    if(existe){
+                    const encontrado = productos.find((producto)=>{
+                        return producto.nombre === opcion.toLowerCase();
+                      })
+                    alert("Elegiste " + encontrado.nombre.toUpperCase() + " a $" + encontrado.precio);
+                    carrito.push(encontrado);
+                    }else{
+                        alert("Opcion no válida");
+                    }
+                opcion = prompt("Desea agregar algo mas?: \n" + productos[0].nombre.toUpperCase() + ": $" + productos[0].precio + " \n" + productos[1].nombre.toUpperCase() + ": $" + productos[1].precio + " \n" + productos[2].nombre.toUpperCase() + ": $" + productos[2].precio + " \n Presioná X para finalizar.");
+                }
+                carrito.forEach((el) => {
+                    totalCarrito += el.precio;
+                });
+                alert("Tu compra final es de $" + totalCarrito);
             }else{
-                alert("Tu compra es de $ " + compra);
+                alert("Tu compra es de $ " + totalCarrito);
                 alert("Gracias por confiar en nosotros")
             }
             break;
